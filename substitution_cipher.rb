6d23d579df3fab1e26ebac26016415fb511e113d
Fong <fong54388@gmail.com>
2023-03-14 11:32:23 +0800

    Permutation

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -27,7 +27,11 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      rand_key = (0..127).to_a.shuffle(random: Random.new(key))
+      result = document.to_s.split('').map do |e|
+        rand_key[e.ord].chr
       end
+      result.join
     end
 
     # Decrypts String document using integer key
@@ -37,7 +41,11 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      rand_key = (0..127).to_a.shuffle(random: Random.new(key))
+      result = document.to_s.split('').map do |d|
+        rand_key.index(d.ord).chr
+      end
+      result.join
     end
   end
 end
-
