ebfd43b10df7c934758de3ac08cb74d9305c0d5d
Anita <yachi77128230@gmail.com>
2023-03-14 20:40:47 +0800

    update substitution_cipher.rb

--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,5 @@
 module SubstitutionCipher
+  # module Caesar works to shift every input letter their position according to key
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -7,6 +8,11 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      result_key = key > 26 ? key - 26 : key
+      # result = document.to_s.chars.map { |c| c.ord }
+      # result = document.to_s.bytes
+      result = document.to_s.bytes.map { |char| char + result_key }
+      result.map(&:chr).join
     end
 
     # Decrypts String document using integer key
@@ -16,9 +22,15 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      result_key = key > 26 ? key - 26 : key
+      # result = document.to_s.chars.map { |c| c.ord }
+      # result = document.to_s.bytes
+      result = document.to_s.bytes.map { |char| char - result_key }
+      result.map(&:chr).join
     end
   end
 
+  # module Permutation works to randomly shuffle every input letter according to key
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -27,7 +39,11 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      rand_key = (0..127).to_a.shuffle(random: Random.new(key))
+      result = document.to_s.split('').map do |e|
+        rand_key[e.ord].chr
       end
+      result.join
     end
 
     # Decrypts String document using integer key
@@ -37,7 +53,11 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      rand_key = (0..127).to_a.shuffle(random: Random.new(key))
+      result = document.to_s.split('').map do |d|
+        rand_key.index(d.ord).chr
+      end
+      result.join
     end
   end
 end
-
